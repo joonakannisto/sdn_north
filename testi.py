b1a5a07cde7aaf55b8fc25ada844457d39f1f570
Joona Kannisto <joona.kannisto@gmail.com>
2015-11-05 14:44:06 +0200

    copy paste error

--- testi.py
+++ testi.py
@@ -4,6 +4,6 @@ import requests
 #add_header(key, val)
 logindata='{"login":{"user":"sdn","password":"skyline","domain":"sdn"}}'
 
-req = requests.post('https://sdn.mi.sec.rd.tut.fi:8443/api/auth', data=json.dumps(payload), verify='sdncertti')
+req = requests.post('https://sdn.mi.sec.rd.tut.fi:8443/api/auth', data=json.dumps(logindata), verify='sdncertti')
 
 print req
