bbdcd41c0e26badab059a2c0710d83b380182de5
Joona Kannisto <joona.kannisto@gmail.com>
2015-11-05 15:33:33 +0200

    almost there

--- testi.py
+++ testi.py
@@ -1,13 +1,18 @@
 #!/usr/bin/env python
 import json
 import requests
-#add_header(key, val)
-logindata={"login":{"user":"sdn","password":"skyline","domain":"sdn"}}
-headers = {'Content-Type': 'application/json'}
-req = requests.post('https://130.230.115.203:8443/sdn/v2.0/auth', headers=headers, data=json.dumps(logindata), verify='sdncertti')
-# make an exectpion if not 200 ok
-req.raise_for_status()
-# it loads json from the string
-authtoken = json.loads(req.text)
-token = authtoken["record"]["token"]
-print token
+
+
+def get_token(logindata):
+    headers = {'Content-Type': 'application/json'}
+    req = requests.post('https://130.230.115.203:8443/sdn/v2.0/auth', headers=headers, data=json.dumps(logindata), verify='sdncertti')
+    # make an exectpion if not 200 ok
+    req.raise_for_status()
+    # it loads the json from the string
+    authtoken = json.loads(req.text)
+    token = authtoken["record"]["token"]
+    return token
+
+
+login={"login":{"user":"sdn","password":"skyline","domain":"sdn"}}
+token = get_token(login)
