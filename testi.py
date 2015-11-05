178d9b350bf6a3e8cc91ded29bed4ecaaa85a51d
Joona Kannisto <joona.kannisto@gmail.com>
2015-11-05 14:24:18 +0200

    authentication

--- testi.py
+++ testi.py
@@ -1,4 +1,11 @@
+#!/usr/bin/env python
 import json
 import urllib2
+#add_header(key, val)
+logindata='{"login":{"user":"sdn","password":"skyline","domain":"sdn"}}'
 
+req = urllib2.Request('https://sdn.mi.sec.rd.tut.fi:8443/api/auth')
+req.add_header('Content-Type', 'application/json')
+response = urllib2.urlopen(req, json.dumps(logindata))
 
+print response
