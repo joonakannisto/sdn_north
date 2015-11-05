28df50b6f06391d1681a0057e90eddb7f3f2b0ea
Joona Kannisto <joona.kannisto@gmail.com>
2015-11-05 15:13:45 +0200

    correct url

--- testi.py
+++ testi.py
@@ -7,4 +7,4 @@ headers = {'Content-Type': 'application/json'}
 req = requests.post('https://130.230.115.203:8443/sdn/v2.0/auth', headers=headers, data=json.dumps(logindata), verify='sdncertti')
 # make an exectpion if not 200 ok
 req.raise_for_status()
-authtoken = json.load(req.text)
+authtoken = json.load(req.text())
