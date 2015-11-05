985fa61a6d58ffab154d649fbd814ea3f9719d0d
Joona Kannisto <joona.kannisto@gmail.com>
2015-11-05 15:52:37 +0200

    almost there

--- testi.py
+++ testi.py
@@ -13,7 +13,16 @@ def get_token(logindata):
     token = authtoken["record"]["token"]
     return token
 
+def get_datapaths(token)
+    headers = {'Content-Type': 'application/json', 'X-Auth-Token' : token}
+    req requests.get('https://130.230.115.203:8443/sdn/v2.0/of/datapaths', headers=headers, verify='sdncertti')
+    # make an exectpion if not 200 ok
+    req.raise_for_status()
+    return req.text
 
 login={"login":{"user":"sdn","password":"skyline","domain":"sdn"}}
 token = get_token(login)
 print token
+# Now use the token inside a X-Auth:
+datapaths = get_datapaths(token)
+print datapaths
