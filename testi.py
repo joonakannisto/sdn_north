821c10d0c739d3769c70e79615fa05ea0559be76
Joona Kannisto <joona.kannisto@gmail.com>
2015-11-05 14:47:18 +0200

    correct url

--- testi.py
+++ testi.py
@@ -4,6 +4,6 @@ import requests
 #add_header(key, val)
 logindata='{"login":{"user":"sdn","password":"skyline","domain":"sdn"}}'
 
-req = requests.post('https://130.230.115.203:8443/api/auth', data=json.dumps(logindata), verify='sdncertti')
+req = requests.post('https://130.230.115.203:8443/sdn/v2.0/auth', data=json.dumps(logindata), verify='sdncertti')
 
 print req
