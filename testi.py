3adbb3329c952fa63ba90743c9adae38a103cd59
Joona Kannisto <joona.kannisto@gmail.com>
2015-11-11 13:15:01 +0200

    put new flow

--- testi.py
+++ testi.py
@@ -122,8 +122,9 @@ template ="""{
 }"""
 flowtemp = json.loads(template)
 flowtemp["flow"]["match"][0]["ipv4_src"]=kohde
-newinstruction = json.loads('{"apply_actions": [{"set_field": {"eth_src":"66.66.66.66.66.66"}},{"output": 26}]}')
-newinstruction["apply_actions"]["output"]=int(forward_path["path"]["links"][0]["src_port"])
+rewsrc="66.66.66.66.66.66"
+newinstruction = json.loads('{"apply_actions": [{"set_field": {"eth_src":'+rewsrc+'}},{"output":'+forward_path["path"]["links"][0]["src_port"])+'}]}')
+newinstruction["apply_actions"].append({"output", int(forward_path["path"]["links"][0]["src_port"])})
 flowtemp["flow"]["instructions"].append(newinstruction)
 
 print json.dumps(flowtemp, sort_keys=True,indent=4)
