df2002b06e04e19e939f7025c64460998e7addb0
Joona Kannisto <joona.kannisto@gmail.com>
2015-11-11 20:02:08 +0200

    refactoring code

---
+++ actions.py
@@ -0,0 +1,95 @@
+import json
+import requests
+login={"login":{"user":"sdn","password":"skyline","domain":"sdn"}}
+host = "https://130.230.115.203:8443"
+
+def qheader(token):
+    return {'Content-Type': 'application/json', 'X-Auth-Token' : token}
+
+def get_token(logindata):
+    headers = {'Content-Type': 'application/json'}
+    req = requests.post(host+'/sdn/v2.0/auth', headers=headers, data=json.dumps(logindata), verify='sdncertti')
+    # make an exectpion if not 200 ok
+    req.raise_for_status()
+    # it loads the json from the string
+    authtoken = json.loads(req.text)
+    token = authtoken["record"]["token"]
+    return token
+
+def get_datapaths(token):
+    req =requests.get(host+'/sdn/v2.0/of/datapaths', headers=qheader(token), verify='sdncertti')
+    req.raise_for_status()
+    dpidt=json.loads(req.text)
+    retarr = []
+    # get rid of completely useless outer key
+    for dpen in dpidt["datapaths"]:
+        retarr.append(dpen["dpid"])
+    return retarr
+
+# T
+def get_nodes(token):
+    req = requests.get(host+'/sdn/v2.0/net/nodes', headers=qheader(token), verify='sdncertti')
+    req.raise_for_status()
+    return req.text
+
+#GET /sdn/v2.0/of/datapaths/{dpid}/flows
+def get_flows(dpid,token):
+    req = requests.get(host+'/sdn/v2.0/of/datapaths/'+dpid+'/flows', headers=qheader(token), verify='sdncertti')
+    req.raise_for_status()
+    return req.text
+
+def dpid_from_ip(ip,token):
+    end_devices=get_nodes(token)
+    devices_list=json.loads(end_devices)
+    if 'nodes' not in devices_list:
+        raise ValueError("Not a nodes list")
+    if 'ip' not in devices_list["nodes"][0]:
+        raise ValueError("No valid data for any target")
+    for node in devices_list["nodes"]:
+        if (node["ip"] == ip ):
+            return node["dpid"]
+    return ""
+
+def ether_from_ip(ip,token):
+    end_devices=get_nodes(token)
+    devices_list=json.loads(end_devices)
+    if 'nodes' not in devices_list:
+        raise ValueError("Not a nodes list")
+    if 'ip' not in devices_list["nodes"][0]:
+        raise ValueError("No valid data for any target")
+    for node in devices_list["nodes"]:
+        if (node["ip"] == ip ):
+            return node["mac"]
+
+
+def find_inport(flowit,ip):
+    flowsj = json.loads(flowit)
+    for flowentry in flowjs["flows"]:
+        if 'match' in flowentry:
+            for rule in flowentry["match"]:
+                if(rule["ipv4_src"]==ip):
+                    for port_rule in flowentry["match"]:
+                        if 'port' in port_rule:
+                            return port_rule["port"]
+
+def flowsforip(flowit,ip)
+flowtemp=json.loads(flowit)
+oldflow=[]
+for flowentry in flowtemp["flows"]:
+    if 'match' in flowentry:
+        for rule in flowentry["match"]:
+            if 'ipv4_src' in rule:
+                if(rule["ipv4_src"]==kohde):
+                    oldflow.append(flowentry)
+                    break
+return oldflow
+
+def get_forward_path(src_dpid,dst_dpid,token):
+    req = requests.get(host+'/sdn/v2.0/net/paths/forward?src_dpid='+src_dpid+'&dst_dpid='+dst_dpid+'', headers=qheader(token), verify='sdncertti')
+    req.raise_for_status()
+    return req.text
+
+def addjsonflow(flow,dst_dpid,token):
+    req = requests.post(host+'/sdn/v2.0/of/datapaths/'+dst_dpid+'/flows', headers=qheader(token), data=flow, verify='sdncertti')
+    req.raise_for_status()
+    return req
---
+++ hairpin.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import json
+import actions
+import hmac
+
+kohde= "130.230.115.233"
+monitor_dpid="00:02:3c:a8:2a:47:d9:80"
+monitor_port ="23"
+
+template ="""{"flow": {
+"cookie": "0x1751985",
+"table_id": 100,
+"priority": 30000,
+"idle_timeout": 300,
+"hard_timeout": 300,
+"match": [],
+"instructions": []}}"""
+token = get_token(login)
+
+target_dpid=dpid_from_ip(kohde,token)
+flowit=get_flows(target_dpi,token)
+
+oldflows=flowsforip(flowit,ip)
+
+# we don't want to raise any errors here
+if (target_dpid !=monitor_dpid):
+    polku=get_forward_path(target_dpid,monitor_dpid,token)
+else:
+    polku = '{"path": {"links" : []}}'
+forward_path=json.loads(polku)
+# Aseta flowt, eli korvaa alkuperäinen kohdeportti forward path ekalla ja toisessa
+# dpid:ssä aseta sisääntuleva liikenne portissa x
+
+
+uusmac="66:66:66:66:66:66"
+origsrc=ether_from_ip(ip,token)
+templateflow=json.loads(template)
+def hairpin(oldflow,forwardpath,ip,target_sw,target_port_in,target_port_out,rewsrc,token):
+    firstaction = json.loads('{[{"set_field": {"eth_src":"'+rewsrc+'"}},{"output":'+target_port_in+'}]}')
+    inport = find_inport(flowit,ip)
+    match = [{'ipv4_src' : ip }, {'inport' : inport}]
+    templateflow["flow"]["match"]=match
+    loopflow = templateflow
+    # we are in the target switch
+    if forwardpath["path"]["links"]
+        firstaction[0]["output"]=target_port_in
+    templateflow.append('apply_actions':firstaction)
+    firstelement=True
+    previous=1
+    for link in forwardpath["path"]["links"]:
+
+        if not firstelement:
+            loopaction{'output' : int(link["src_port"])
+            loopflow["flow"]["match"].append{'eth_src': rewsrc}
+            loopflow["flow"]["match"].append{'port': previous}
+            loopflow["instructions"]["0"]'apply_actions':loopaction)
+        else :
+            firstaction[1]={'output': int(link["src_port"])}
+            loopflow["instructions"]["0"]'apply_actions':firstaction)
+            firstelement=False
+        addjsonflow(json.dumps(loopflow),link["src_dpid"],token)
+        #lets save the 
+        previous=int(link["dst_port"])
+        match[0]["inport"]=int(link["dst_port"])
+        loopflow["instructions"][0]["apply_actions"]
+    newaction[1]["output"] =int(forward_path["path"]["links"][0]["src_port"])
+    templateflow["flow"]["instructions"][0].append('apply_actions':[])
+    newaction[2]["output"] = newflow["instructions"][0]["apply_actions"][0]
+
+    # does not seem to support more than one action
+newflow["instructions"][0]["apply_actions"]=newaction
+
+flowtemp = json.loads(template)
+flowtemp["flow"]["match"][0]["ipv4_src"]=kohde
+
+newinstruction = json.loads('{"apply_actions": [{"set_field": {"eth_src":"'+rewsrc+'"}},{"output":23},{"output":42}]}')
+
+
+flowtemp["flow"]["instructions"][0]=newinstruction
+
+print json.dumps(flowtemp, sort_keys=True,indent=4)
+
+#print addjsonflow(json.dumps(flowtemp),target_dpi,token)
+
+
+#for dpid in datapathids:
+#    print json.dumps(json.loads(get_flows(dpid,token)), indent=4, sort_keys=True)
