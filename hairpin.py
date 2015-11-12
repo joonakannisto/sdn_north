19cd349fc0513b41e8d01f4e00a640a0c0f86d00
Joona Kannisto <joona.kannisto@gmail.com>
2015-11-12 14:41:45 +0200

    modified readme to include descriptions

--- hairpin.py
+++ hairpin.py
@@ -41,6 +41,7 @@ def hairpin(ip,target_sw,target_port_in,target_port_out,rewsrc,token):
     # we are in the target switch
     #if forwardpath["path"]["links"]
     #    firstaction[0]["output"]=target_port_in
+    templateflow=json.loads(template)
     templateflow.append({'apply_actions':firstaction})
     firstelement=True
     previous=actions.find_inport(flowit,ip)
